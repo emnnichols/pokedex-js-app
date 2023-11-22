dc31e8461a79810db502b1f0885d4df34cbb0168
Emily N <emnnichols@outlook.com>
2023-11-22 12:43:52 -0800

    Created pokemon list array
    For exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello world!');
-
-let favoriteFood = 'Korean BBQ';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        weight: 6.9,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1.0,
+        weight: 13.0,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2.0,
+        weight: 100.0,
+        type: ['grass','poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        weight: 8.5,
+        type: ['fire']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        weight: 19.0,
+        type: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        weight: 90.5,
+        type: ['fire','flying']
+    },
+];
