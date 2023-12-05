bc5eb1d04dfd426f2d5ce8f892d3aff67ff771de
Emily N <emnnichols@outlook.com>
2023-12-04 19:24:40 -0800

    Added IIFE
    Exercise 1.5, task part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-let pokemonList = [
-    { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass','poison'] },
-    { name: 'Ivysaur', height: 1.0, weight: 13.0, type: ['grass','poison'] },
-    { name: 'Venusaur', height: 2.0, weight: 100.0, type: ['grass','poison'] },
-    { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
-    { name: 'Charmeleon', height: 1.1, weight: 19.0, type: ['fire'] },
-    { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire','flying'] },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass','poison'] },
+        { name: 'Ivysaur', height: 1.0, weight: 13.0, type: ['grass','poison'] },
+        { name: 'Venusaur', height: 2.0, weight: 100.0, type: ['grass','poison'] },
+        { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
+        { name: 'Charmeleon', height: 1.1, weight: 19.0, type: ['fire'] },
+        { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire','flying'] },
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    
+    function getAll() {
+        return pokemonList;
+    }
+    
+      return {
+        add: add,
+        getAll: getAll
+      };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     let writeHeight = pokemon.height >= 1.0
         ? `Height: ${pokemon.height} m - Wow, that's big!`
         : `Height: ${pokemon.height} m`;
