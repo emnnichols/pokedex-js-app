9fb08e2fd4dc4bc3366b91ff0aaf7367d0b82577
Emily N <emnnichols@outlook.com>
2023-12-04 18:36:11 -0800

    Replaced for loop with forEach function
    Exercise 1.5, task part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,19 @@
 let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        weight: 6.9,
-        type: ['grass','poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 1.0,
-        weight: 13.0,
-        type: ['grass','poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2.0,
-        weight: 100.0,
-        type: ['grass','poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        weight: 8.5,
-        type: ['fire']
-    },
-    {
-        name: 'Charmeleon',
-        height: 1.1,
-        weight: 19.0,
-        type: ['fire']
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        weight: 90.5,
-        type: ['fire','flying']
-    },
+    { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['grass','poison'] },
+    { name: 'Ivysaur', height: 1.0, weight: 13.0, type: ['grass','poison'] },
+    { name: 'Venusaur', height: 2.0, weight: 100.0, type: ['grass','poison'] },
+    { name: 'Charmander', height: 0.6, weight: 8.5, type: ['fire'] },
+    { name: 'Charmeleon', height: 1.1, weight: 19.0, type: ['fire'] },
+    { name: 'Charizard', height: 1.7, weight: 90.5, type: ['fire','flying'] },
 ];
 
-for (let i=0; i < pokemonList.length; i++) {
-    let pokemonName = pokemonList[i].name;
-    let pokemonHeight = pokemonList[i].height;
-    let pokemonWeight = pokemonList[i].weight;
-    let writeHeight = pokemonHeight >= 1.0 
-        ? `Height: ${pokemonHeight} m - Wow, that's big!` 
-        : `Height: ${pokemonHeight} m`;
-    document.write(`<h2>${pokemonName}</h2> <p class="pokemon-info">${writeHeight} <br> Weight: ${pokemonWeight} kg </p>`);
-}
+pokemonList.forEach(function(pokemon) {
+    let writeHeight = pokemon.height >= 1.0
+        ? `Height: ${pokemon.height} m - Wow, that's big!`
+        : `Height: ${pokemon.height} m`;
+    document.write(
+        `<h2>${pokemon.name}</h2> 
+        <p class="pokemon-info">${writeHeight} <br> 
+        Weight: ${pokemon.weight} kg</p>`
+        );
+});
